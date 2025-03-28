e074d3f3e6b1092db49e68be99cb0dcbe2d2778e
tamarachapeton <tamarachapeton1525@gmail.com>
2025-03-27 22:27:13 -0400

    modificar y arrege bugs

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const multiplicador  =  ( x ,  y )  =>  { return  x  *  y  } ;
 function  calcularMultiplicacion ( a ,  b , multiplicador ) { 
     let  resultado  =  multiplicador ( a ,  b ) ;
 
-    consola . log ( `Resultado de la multiplicación: ${resultado} `) ;
+    console . log ( `Resultado de la multiplicación: ${resultado} `) ;
 }
 
-calcularMultiplicacion ( 3 ,  7, ) ; 
+calcularMultiplicacion ( 3 ,  7, multiplicador) ; 
 // Resultado de la multiplicación: 21
